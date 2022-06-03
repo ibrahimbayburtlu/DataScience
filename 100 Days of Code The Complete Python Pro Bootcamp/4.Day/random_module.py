68122f7a9a4c4e1cf9591eea85f1e67a7b132577
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-03 14:28:35 +0300

    Day 4 - Random Module

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/4.Day/random_module.py
@@ -0,0 +1,16 @@
+import random
+
+# How to create random number (int)
+random_number = random.randint(1, 10)
+print(random_number)
+
+# How to create random number (float)
+random_float = random.random()
+print(random_float)
+
+# How to create random number (decimal)
+random_decimal = random.uniform(10.5, 22.2)
+print(random_decimal)
+
+your_love_score = random.randint(1,100)
+print(f"Your love score is {your_love_score}.")
