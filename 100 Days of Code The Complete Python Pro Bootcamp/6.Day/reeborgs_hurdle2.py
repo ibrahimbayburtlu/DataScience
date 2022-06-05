14567da6dff3c5882410e11c1cdaa035707779de
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-05 16:57:02 +0300

    Day 6 - Reeborgs Hurt 2 Updated

--- 100 Days of Code The Complete Python Pro Bootcamp/6.Day/reeborgs_hurdle2.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/6.Day/reeborgs_hurdle2.py
@@ -1,9 +1,9 @@
-
 def turn_around():
     for i in range(3):
         turn_left()
 #  at_goal is target I want to arrive my goal.
-while not at_goal():
+
+def repeat():
     move()
     turn_left()
     move()
@@ -11,3 +11,7 @@ while not at_goal():
     move()
     turn_around()
     move()
+    turn_left()
+
+while not at_goal():
+    repeat()
