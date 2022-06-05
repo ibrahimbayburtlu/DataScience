db16b9d004a6e23752518956f9a55fba3b03ebfe
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-05 15:00:56 +0300

    Day 6 - Reeborgs Robots

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/6.Day/reeborgs_world.py
@@ -0,0 +1,33 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+for i in range(0,2):
+    move()
+turn_right()
+move()
+turn_left()
+for i in range(0,4):
+    move()
+turn_left()
+for i in range(0,4):
+    move()
+turn_left()
+for i in range(0,5):
+    move()
+turn_right()
+for i in range(0,5):
+    move()
+turn_left()
+for i in range(0,4):
+    move()
+turn_left()
+for i in range(0,9):
+    move()
+turn_left()
+for i in range(0,3):
+    move()
