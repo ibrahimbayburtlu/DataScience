2b24c619160f4a71abf72cb79831eef0ddb4d4a9
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-06 16:40:15 +0300

    Day 7 - Hangman Step 2 Fix Bug

--- 100 Days of Code The Complete Python Pro Bootcamp/7.Day/Step2.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/7.Day/Step2.py
@@ -3,7 +3,7 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+word_length = len(chosen_word)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -11,7 +11,10 @@ print(f'Pssst, the solution is {chosen_word}.')
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 guess = input("Guess a letter: ").lower()
-display = ['_' for i in range(len(chosen_word))]  
+display = []
+for _ in range(word_length):
+    display += "_"
+
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
