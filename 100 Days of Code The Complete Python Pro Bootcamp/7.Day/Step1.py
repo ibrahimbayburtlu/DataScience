bbf334c4a4cf483cb954e40b8d0fa33668396b44
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-06 15:03:58 +0300

    Day 7 - Hangman Step 1

--- 100 Days of Code The Complete Python Pro Bootcamp/7.Day/Step1.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/7.Day/Step1.py
@@ -4,7 +4,7 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-chosen_word = word_list[random.randint(0,2)]
+chosen_word = random.choice(word_list)
 print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
