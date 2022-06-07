0dea3c6783af084ab8ed3db30db9cf8521aca349
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-07 12:27:43 +0300

    Day 8 - Start

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/8.Day/Day8_start.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet(par1,par2,par3):
+    print(f"first parametre:{par1}")
+    print("second parametre:{}".format(par2))
+    print("third parametre:"+ str(par3))
+
+greet(10,20,30)
