b9cce83ea4ac5f660d32a7d53291d4b8c4939162
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-11 16:16:04 +0300

    Day 13 - Exercise 1

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/13.Day/ex1.py
@@ -0,0 +1,16 @@
+#Read this the code in main.py
+#Spot the problems 🐞.
+#Modify the code to fix the program
+
+number = int(input("Which number do you want to check?"))
+'''
+if number % 2 = 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+'''
+# fix bug
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
