3f3e1f946c7d04da4f60cd92fe00fcb0a50c57be
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-13 10:49:16 +0300

    HackerRank introduction Exercise - 7

---
+++ HackerRank/introduction/ex7.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    string_expression = ""
+    for i in range(1,n+1):
+        string_expression += str(i)
+    print(string_expression) 
+        
