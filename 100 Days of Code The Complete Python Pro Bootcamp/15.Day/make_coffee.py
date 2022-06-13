82f210d94f9ee44ba52c2d783677edeb793676a1
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-13 10:32:06 +0300

    Day 15 - Make Coffee machine

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/15.Day/make_coffee.py
@@ -0,0 +1,44 @@
+from secrets import choice
+
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+money = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+flag = True
+while flag:
+    choice = input("What would you like(espresso/latte/cappuccino):")
+    if choice == "off":
+        flag = False
+    elif choice = "report":
+        print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${money}")
+
