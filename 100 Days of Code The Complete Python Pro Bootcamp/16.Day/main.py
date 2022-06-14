fe81a84f8f8a3df9c6183d370bc8ef2bcfd21a3c
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-14 14:40:29 +0300

    Day 16 - OOP Coffee Machine

--- 100 Days of Code The Complete Python Pro Bootcamp/16.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/16.Day/main.py
@@ -1,19 +1,3 @@
-import turtle
-import another_module
-print(another_module.another_variable)
-
-from turtle import Turtle, Screen
-
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-
-my_screen = Screen()
-print(my_screen.canvheight)
-timmy.color("red")
-timmy.forward(100)
-my_screen.exitonclick()
-
-
-import prettytable
-
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
---
+++ 100 Days of Code The Complete Python Pro Bootcamp/16.Day/module_start.py
@@ -0,0 +1,19 @@
+import turtle
+import another_module
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+timmy.color("red")
+timmy.forward(100)
+my_screen.exitonclick()
+
+
+import prettytable
+
