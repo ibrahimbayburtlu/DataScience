01e2f77b7912e057596020b8c5875c3822875b9b
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-14 11:32:42 +0300

    Day 16 - Turtle Module start

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/16.Day/main.py
@@ -0,0 +1,13 @@
+import another_module
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+timmy.color("red")
+my_screen.exitonclick()
