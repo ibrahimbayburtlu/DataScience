ae13c0fab8d3e18181801d6ac0111a1335da72e2
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-15 21:48:35 +0300

    Day 17 - Quiz Game Step 2

--- 100 Days of Code The Complete Python Pro Bootcamp/17.Day/question_model.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/17.Day/question_model.py
@@ -1,7 +1,7 @@
-class question():
+class Question():
     def __init__(self,text,answer):
         self.text = text
         self.answer = answer
 
-p1 = question("2+3=5","True")
+p1 = Question("2+3=5","True")
 print(p1.answer)
