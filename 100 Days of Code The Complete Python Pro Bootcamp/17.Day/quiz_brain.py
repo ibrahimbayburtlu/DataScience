6f9e1ed170b451ca7c9ccae4424a8eb1a2c9fe87
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-15 22:41:32 +0300

    Day 17 - Quiz Game Finish

--- 100 Days of Code The Complete Python Pro Bootcamp/17.Day/quiz_brain.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/17.Day/quiz_brain.py
@@ -2,13 +2,11 @@ from typing_extensions import Self
 
 
 class QuizBrain():
-    def __init__(self,question_number,question_list):
+    def __init__(self, q_list):
         self.question_number = 0
-        self.list = question_list
+        self.question_list = q_list
 
     def next_question(self):
-        current_question = self.list[self.question_number]
+        current_question = self.question_list[self.question_number]
         self.question_number += 1
         input(f"Q.{current_question}:{current_question.text}(True/False)")
-
-
