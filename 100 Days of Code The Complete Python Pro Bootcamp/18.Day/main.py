28340e9b7dab4975135e4269828759f25888b5d4
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-16 23:05:37 +0300

    Day 18 - Turtle Learning

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/18.Day/main.py
@@ -0,0 +1,16 @@
+from turtle import Screen, Turtle
+timmy_the_turtle  = Turtle()
+
+
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.position()
+timmy_the_turtle.forward(100)
+timmy_the_turtle.position()
+timmy_the_turtle.right(98)
+timmy_the_turtle.backward(30)
+timmy_the_turtle.setx(10)
+timmy_the_turtle.sety(20)
+screen = Screen()
+screen.exitonclick()
