8368ae2ff6dc5f1dbc94eb1ca184037cef65d1db
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-16 23:12:06 +0300

    Day 18 - Turtle Challenge 1- Draw a Square

--- 100 Days of Code The Complete Python Pro Bootcamp/18.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/18.Day/main.py
@@ -3,14 +3,9 @@ timmy_the_turtle  = Turtle()
 
 
 
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.position()
-timmy_the_turtle.forward(100)
-timmy_the_turtle.position()
-timmy_the_turtle.right(98)
-timmy_the_turtle.backward(30)
-timmy_the_turtle.setx(10)
-timmy_the_turtle.sety(20)
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
 screen = Screen()
 screen.exitonclick()
