f4b89c6f5b13fb47caa06611cf11632db920f485
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-17 11:06:45 +0300

    Update

--- 100 Days of Code The Complete Python Pro Bootcamp/18.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/18.Day/main.py
@@ -1,10 +1,5 @@
 from turtle import Screen, Turtle
 timmy_the_turtle  = Turtle()
-# import turtle
-# timmy_the_turtle = turtle.Turtle()
-
-# import turtle as t
-# tim = t.Turtle()
 
 for i in range(4):
     timmy_the_turtle.forward(100)
