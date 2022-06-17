d9b9598d34dae1fdea0c27f2379e64911c81323f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-17 11:06:15 +0300

    Day 18 - Challenge 2

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/18.Day/day18-start-2.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+
+
+for i in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown
+screen = t.Screen()
+screen.exitonclick()
