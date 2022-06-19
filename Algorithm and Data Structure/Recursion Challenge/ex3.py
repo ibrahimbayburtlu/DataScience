e337e86ca13214663eba93fc1b261bac062e591f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-19 15:08:22 +0300

    Recursive Problems

--- Algorithm and Data Structure/Recursion Challenge/ex3.py
+++ Algorithm and Data Structure/Recursion Challenge/ex3.py
@@ -1,4 +1,6 @@
-def productOfArray(arr):
+def productofarray(arr):
     if len(arr) == 0:
         return 1
-    return arr[0] * productOfArray(arr[1:])
+    return arr[0] * productofarray(arr[1:])
+
+print(productofarray(arr = [2,3,4,6,7,8,9,10]))
