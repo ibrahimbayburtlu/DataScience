ad1c3f01e24631a076b03355bdb3145b985bb81e
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-21 13:49:28 +0300

    Pong Game - Step 2

--- 100 Days of Code The Complete Python Pro Bootcamp/22.Day/pong_game.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/22.Day/pong_game.py
@@ -1,10 +1,30 @@
 from turtle import Turtle,Screen
-
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("Pong Game")
+screen.tracer()
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.turtlesize(5,1)
+paddle.penup()
+paddle.goto(x=350,y=0)
 
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(),new_y)
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(),new_y)
+screen.listen()
+screen.onkey(go_up,"Up")
+screen.onkey(go_down,"Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
