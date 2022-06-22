73ca4d4d7329b822653247aaa0903989da21498a
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-22 22:49:37 +0300

    Day 23 - Cross Game Step - 2

--- 100 Days of Code The Complete Python Pro Bootcamp/23.Day/car_manager.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/23.Day/car_manager.py
@@ -1,7 +1,22 @@
+from importlib.util import set_loader
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-
-class CarManager:
-    pass
+random_positions = random.randint(0,300)
+random_color = random.randint(0,5)
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color(COLORS[random_color])
+        self.shapesize(3,1)
+        self.penup()
+        self.goto(300,random_positions)
+    
+    def move(self):
+        new_x = self.xcor() + STARTING_MOVE_DISTANCE
+        self.goto(new_x,self.ycor())
