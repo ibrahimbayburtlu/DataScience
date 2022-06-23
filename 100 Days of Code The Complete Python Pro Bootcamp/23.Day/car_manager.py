ab2cb823401da4cfaec3e5715e69ee13bb8b50b9
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-23 13:44:31 +0300

    Day 23 - Cross Game

--- 100 Days of Code The Complete Python Pro Bootcamp/23.Day/car_manager.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/23.Day/car_manager.py
@@ -1,4 +1,3 @@
-from importlib.util import set_loader
 from turtle import Turtle
 import random
 
@@ -6,17 +5,27 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-random_positions = random.randint(0,300)
-random_color = random.randint(0,5)
-class CarManager(Turtle):
+
+class CarManager:
+
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.color(COLORS[random_color])
-        self.shapesize(3,1)
-        self.penup()
-        self.goto(300,random_positions)
-    
-    def move(self):
-        new_x = self.xcor() + STARTING_MOVE_DISTANCE
-        self.goto(new_x,self.ycor())
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
--- 100 Days of Code The Complete Python Pro Bootcamp/23.Day/scoreboard.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/23.Day/scoreboard.py
@@ -1,21 +1,26 @@
 from turtle import Turtle
-from typing_extensions import Self
-
 
 FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
-        self.color("Black")
-        self.penup()
+        self.level = 1
         self.hideturtle()
-        self.score = 0
+        self.penup()
+        self.goto(-280, 250)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(-270,270)
-        self.write(self.score, align="center",font=FONT)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
 
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
