5f083e5e3c2e8cbbaf2cbbde7dc8478538b3a4b8
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-26 21:27:32 +0300

    Day 25

--- 100 Days of Code The Complete Python Pro Bootcamp/25.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/25.Day/main.py
@@ -28,3 +28,28 @@ print(temp_list)
 average = sum(temp_list) / 7
 print(average)
 
+print(data["temp"].max())
+
+# Get Data in Columns
+print(data["condition"])
+print(data.condition)
+
+# Get Data in Row
+print(data[data.day == "Monday"]) 
+
+print(data[data.temp == max(data.temp)])
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+monday_temp_F =monday_temp * 9/5 + 32
+print(monday_temp_F)
+
+# Create a dataframe from scratch 
+
+data_dict = {
+    "students":["Any","James","Angela"],
+    "scores":[76,56,65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
