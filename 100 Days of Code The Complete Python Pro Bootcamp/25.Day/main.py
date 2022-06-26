2dbde1871d2b954da559fdae68501c826ef4fe72
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-26 19:34:44 +0300

    Day 25

--- 100 Days of Code The Complete Python Pro Bootcamp/25.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/25.Day/main.py
@@ -2,7 +2,8 @@ with open("weather_data.csv") as data_files:
     data = data_files.read()
     print(data)
 
-import csv 
+import csv
+from numpy import average 
 
 with open("weather_data.csv") as data_files:
     data = csv.reader(data_files)
@@ -17,3 +18,13 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 print(data["day"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+average = sum(temp_list) / 7
+print(average)
+
