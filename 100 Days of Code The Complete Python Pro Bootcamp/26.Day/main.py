7db028a49c96d53d82fd90169f55443704398b9e
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-27 23:47:28 +0300

    Day 26

--- 100 Days of Code The Complete Python Pro Bootcamp/26.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/26.Day/main.py
@@ -1,57 +1,15 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-numbers = [1,2,3]
-newNumbers = [i+1 for i in numbers]
-print(newNumbers)
+import pandas
 
-name = "Angela"
-new_list = [letter for letter in name]
-print(new_list)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-new_number_list = [ i*i for i in range(1,5)]
-#for i in range(1,5):
-    #new_number = i * i 
-    #new_number_list.append(new_number)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-print(new_number_list)
-
-names = ["Alex","Beth","Caroline","Dave","Elanor","Freddle"]
-
-# sort_names  = [new_item for item in list if test]
-sort_names = [i for i in names if len(i) < 5]
-print(sort_names)
-
-sort_names = [i.upper() for i in names if len(i) > 5]
-print(sort_names)
-
-from multiprocessing.sharedctypes import Value
-import random 
-names = ["Alex","Beth","Caroline","Dave","Elanor","Freddle"]
-
-students_scores = {student:random.randint(1,100) for student in names}
-print(students_scores)
-
-passed_students = { student:score for (student,score) in students_scores.items() if score >= 60}
-print(passed_students)
-
-students_dict = {
-    "student":["Angela","James","Lily"],
-    "score":[56,76,98]
-}
-
-for (key,value) in students_dict.items():
-    print(key)
-
-import pandas 
-
-student_data_frame = pandas.DataFrame(students_dict)
-print(student_data_frame)
-
-# Loop through a data frame
-
-#for (key,value) in student_data_frame.items():
-    #print(value)
-
-# Loop through rows of a data frame
-for (index,row) in student_data_frame.iterrows():
-    if row.student == "Angela":
-        print(row.score)
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
