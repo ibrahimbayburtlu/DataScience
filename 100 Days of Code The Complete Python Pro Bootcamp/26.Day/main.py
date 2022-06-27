ec52de4e7d011e41ae9c6902f1e323348716aeb1
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-27 04:00:14 +0300

    Day 26

--- 100 Days of Code The Complete Python Pro Bootcamp/26.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/26.Day/main.py
@@ -13,3 +13,12 @@ new_number_list = [ i*i for i in range(1,5)]
     #new_number_list.append(new_number)
 
 print(new_number_list)
+
+names = ["Alex","Beth","Caroline","Dave","Elanor","Freddle"]
+
+# sort_names  = [new_item for item in list if test]
+sort_names = [i for i in names if len(i) < 5]
+print(sort_names)
+
+sort_names = [i.upper() for i in names if len(i) > 5]
+print(sort_names)
