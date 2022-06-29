07e9301f4aa2dfbfa6b521651a1533c03039c55f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-29 11:17:49 +0300

    Day 27

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/27.Day/main.py
@@ -0,0 +1,20 @@
+from pickle import TRUE
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500,height=300)
+
+
+#Label
+my_label = tkinter.Label(text="I am a Label",font=("Arial",12,"bold"))
+my_label.pack(side="left")
+
+import turtle
+
+tim = turtle.Turtle()
+tim.write("Some Text")
+
+
+
+window.mainloop()
