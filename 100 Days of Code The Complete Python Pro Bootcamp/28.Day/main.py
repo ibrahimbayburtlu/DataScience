4f9ca2e1b3c00735207b3ff511efbd0288553bd4
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-29 19:54:47 +0300

    Day 28

--- 100 Days of Code The Complete Python Pro Bootcamp/28.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/28.Day/main.py
@@ -1,4 +1,5 @@
-
+from _tkinter import *
+from tkinter import Canvas, PhotoImage, Tk
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +17,20 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodro")
+window.config(padx=100,pady=50,bg=YELLOW,highlightthickness=0)
+
+canvas = Canvas(width=200,height=224)
+tomato_ing = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato_ing)
+canvas.create_text(103,112,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+canvas.pack()
+
+
+
+
+
+window.mainloop()
+
