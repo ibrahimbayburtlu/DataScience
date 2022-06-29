64d3b3884b8c861f549dddc40cfc8c8946f23fd4
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-29 14:37:31 +0300

    Day 27

--- 100 Days of Code The Complete Python Pro Bootcamp/27.Day/playground.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/27.Day/playground.py
@@ -1,32 +1,33 @@
-from multiprocessing import Value
-from pyexpat import model
-from numpy import multiply
-
-
+# *args: Positional Variable-Length Arguments
 def add(*args):
-    print(sum(args))
+    # print(args[1])
 
-add(2,3,4,5,7,6,7,2,1,3,4,6)
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+# print(add(3, 5, 6, 2, 1, 7, 4, 3))
 
-
-def calculate(n,**kwargs):
+# **kwargs: Keyworded Variable-Length Arguments
+def calculate(n, **kwargs):
     print(kwargs)
-    for key,value in kwargs.items():
-        print(key)
-        print(value)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
     n += kwargs["add"]
-    n += kwargs["multiply"]
-    print(n)
+    n *= kwargs["multiply"]
+    # print(n)
 
-calculate(2,add = 3 , multiply = 5)
+calculate(2, add=3, multiply=5)
 
-
+# How to use a **kwargs dictionary safely
 class Car:
-    def __init__(self,**kw):
+
+    def __init__(self, **kw):
         self.make = kw.get("make")
         self.model = kw.get("model")
         self.colour = kw.get("colour")
         self.seats = kw.get("seats")
 
-my_car = Car(make="Nissan",model="Skyline")
+my_car = Car(make="Nissan", model="Skyline")
 print(my_car.model)
