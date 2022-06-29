1482126ebe34a061e653446fb4bf43c19d36bd0e
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-29 14:35:01 +0300

    Day 27

--- 100 Days of Code The Complete Python Pro Bootcamp/27.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/27.Day/main.py
@@ -3,14 +3,14 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500,height=300)
-
+window.config(padx=20,pady=20)
 
 #Label
 my_label = Label(text="I am a Label",font=("Arial",12,"bold"))
-my_label.pack()
 
-my_label["text"] = "New Text"
+
 my_label.config(text="New Text")
+my_label.grid(column=0,row=0)
 
 # Button 
 
@@ -20,11 +20,17 @@ def button_clicked():
     my_label.config(text=new_text)
 
 button = Button(text="Click Me",command=button_clicked)
-button.pack()
+button.grid(column=2,row=2)
+
+new_button = Button(text="Click Me2")
+new_button.grid(column=3,row=1)
+
 
 # Entry
 
 input = Entry(width=10)
-input.pack()
 print(input.get())
+input.grid(column=3,row=2)
+
+
 window.mainloop()
