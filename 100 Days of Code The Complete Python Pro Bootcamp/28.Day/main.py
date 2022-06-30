494ae18854846fbbcfad82545f8da78848e68a8e
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-06-30 13:53:59 +0300

    Day 28

--- 100 Days of Code The Complete Python Pro Bootcamp/28.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/28.Day/main.py
@@ -1,8 +1,11 @@
 from _tkinter import *
-from cgitb import reset
+from cgitb import reset, text
 from tabnanny import check
+import threading
 from tkinter import Button, Canvas, Label, PhotoImage, Tk
 from tracemalloc import start
+from turtle import title
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -17,13 +20,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+
+    count_down(5*60)
+
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60 
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    print(count)
+    if count > 0:
+        window.after(1000,count_down,count-1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodro")
 window.config(padx=100,pady=50,bg=YELLOW)
+
 
 
 title_label = Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME, 50))
@@ -33,8 +53,10 @@ title_label.grid(column=1,row=0)
 canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 tomato_ing = PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato_ing)
-canvas.create_text(103,112,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+timer_text = canvas.create_text(103,112,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
+
+start_timer()
 
 start_button = Button(text="start")
 start_button.grid(column=0,row=2)
