0cd4e1c1586e47b9795b3b619a6c31fc75fe08d1
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-01 17:40:05 +0300

    Day 29

--- 100 Days of Code The Complete Python Pro Bootcamp/29.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/29.Day/main.py
@@ -1,12 +1,7 @@
 from tkinter import *
+from tkinter import messagebox
+from random import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-
-
-
-
-
-
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -14,10 +9,16 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt","a") as data_file:
-        data_file.write(f"{website}|{email}|{password}\n")
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops",message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website,message=f"These are the details entered: \n Email:{email}\n Password:{password}\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt","a") as data_file:
+                data_file.write(f"{website}|{email}|{password}\n")
+                website_entry.delete(0,END)
+                password_entry.delete(0,END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
