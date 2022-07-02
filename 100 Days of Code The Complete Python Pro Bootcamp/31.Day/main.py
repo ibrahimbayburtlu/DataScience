a6259c6615a1daff683690db3f3a46f41be9c0eb
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-02 23:18:01 +0300

    Day 31

--- 100 Days of Code The Complete Python Pro Bootcamp/31.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/31.Day/main.py
@@ -1,7 +1,24 @@
-from email.mime import image
 from tkinter import *
+import pandas 
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("data/french_words.cvs")
+to_learn = data.to_dict(orient="records")
+print(to_learn)
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title,text="French")
+    canvas.itemconfig(card_word,text=current_card["French"])
+
+
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Flash Cards")
@@ -10,19 +27,20 @@ window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263,image=card_front_img)
-canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
-canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
+card_title = canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
+card_word = canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 canvas.grid(row=0,column=0,columnspan=2)
 
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknow_button = Button(image=cross_image,highlightthickness=0)
+unknow_button = Button(image=cross_image,highlightthickness=0,command=next_card)
 unknow_button.grid(row=1,column=0)
 
 check_image = PhotoImage(file="images/right.png")
-know_button = Button(image=check_image,highlightthickness=0)
+know_button = Button(image=check_image,highlightthickness=0,command=next_card)
 know_button.grid(row=1,column=1)
 
+next_card()
 
 window.mainloop()
