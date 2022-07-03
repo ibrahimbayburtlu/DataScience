babbfe55ee16995cf0063a86c62c6143028dcc7a
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-03 15:57:45 +0300

    Day 32

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/32.Day/main.py
@@ -0,0 +1,14 @@
+import smtplib
+
+my_email = "ibrahimbayburtlu5@gmail.com"
+password = "iBo12345*"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls() # use security
+    connection.login(user=my_email,password=password)
+    connection.sendmail(from_addr=my_email,
+    to_addrs="ibrahimbayburtluweb3learning@gmail.com",
+    msg="Subhect:Hello\n\nThis is the body of my email.")
+
+
+
