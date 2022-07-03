422dce1b44884eec4ff3c119b308e48ca82a3b9f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-03 17:52:15 +0300

    Day 32

--- 100 Days of Code The Complete Python Pro Bootcamp/32.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/32.Day/main.py
@@ -1,17 +1,17 @@
-#import smtplib
+import smtplib
 
-''' 
+
 my_email = "ibrahimbayburtlu5@gmail.com"
 password = "iBo12345*"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
+with smtplib.SMTP("smtp.gmail.com",port=535) as connection:
     connection.starttls() # use security
     connection.login(user=my_email,password=password)
     connection.sendmail(from_addr=my_email,
     to_addrs="ibrahimbayburtluweb3learning@gmail.com",
     msg="Subject:Hello\n\nThis is the body of my email.")
 
-'''
+
 
 import datetime as dt
 
