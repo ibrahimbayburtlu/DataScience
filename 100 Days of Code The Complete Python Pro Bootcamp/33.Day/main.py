de7852f9794053c5ec21d68077b0623a74a9e039
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-05 18:32:52 +0300

    33

--- 100 Days of Code The Complete Python Pro Bootcamp/33.Day/main.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/33.Day/main.py
@@ -1,5 +1,10 @@
 import requests
+from datetime import datetime
 
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+'''
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response)
 response.raise_for_status()
@@ -9,3 +14,24 @@ longitude = data["iss_position"]["longitude"]
 latitude = data["iss_position"]["latitude"]
 position = (longitude,latitude)
 print(position)
+'''
+
+parametres={
+    "lat":MY_LAT,
+    "lng":MY_LONG,
+    "formatted":0,
+}
+response = requests.get(url="https://api.sunrise-sunset.org/json",params=parametres)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+
+print(sunrise.split("T")[1].split(":")[0])
+print(sunset.split("T")[1].split(":")[0])
+
+
+
+time_now = datetime.now()
+print(time_now.hour)
