5cf8df1d963e3ac13b7cea7ff8f2b95af7944c5e
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-06 19:27:52 +0300

    Day 34

--- 100 Days of Code The Complete Python Pro Bootcamp/34.Day/data.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/34.Day/data.py
@@ -1,3 +1,21 @@
+import requests
+
+parametres = {
+    "amount":10,
+    "type":"boolean"
+}
+
+
+
+
+
+response = requests.get("https://opentdb.com/api.php",params=parametres)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
+
+'''
 question_data = [
     {
         "category": "Science: Computers",
@@ -100,3 +118,4 @@ question_data = [
         ]
     }
 ]
+'''
