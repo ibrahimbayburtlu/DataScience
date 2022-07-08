8ef95f15ddbd84fbe360887f7a33230e5e6a2988
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-08 16:15:09 +0300

    Recursion Example 11

--- Algorithm and Data Structure/Recursion Challenge/ex11.py
+++ Algorithm and Data Structure/Recursion Challenge/ex11.py
@@ -1,1 +1,31 @@
-print("Hello World!")
+
+# nestedEvenSum Solution
+
+obj1 = {
+  "outer": 2,
+  "obj": {
+    "inner": 2,
+    "otherObj": {
+      "superInner": 2,
+      "notANumber": True,
+      "alsoNotANumber": "yup"
+    }
+  }
+}
+
+obj2 = {
+  "a": 2,
+  "b": {"b": 2, "bb": {"b": 3, "bb": {"b": 2}}},
+  "c": {"c": {"c": 2}, "cc": 'ball', "ccc": 5},
+  "d": 1,
+  "e": {"e": {"e": 2}, "ee": 'car'}
+}
+
+def nestedevensum(obj, sum=0):
+    for key in obj:
+        if type(obj[key]) is dict:
+            sum += nestedevensum(obj[key])
+        elif type(obj[key]) is int and obj[key]%2==0:
+            sum+=obj[key]
+
+print(nestedevensum(obj1))
