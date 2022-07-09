b79210afeaa2f8b85b2201fa580451f74a5257fe
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-09 11:46:59 +0300

    Big O notation

--- Algorithm and Data Structure/Big O/timeComplexities.py
+++ Algorithm and Data Structure/Big O/timeComplexities.py
@@ -50,23 +50,23 @@ for a in arrayA:
 
 sample1Array = [1,10,45,33,23,45,67,2,3,33,55,11,65,76,34,35,27,99]
 
-def findBiggestNumber(sampleArray):
-    biggestNumber = sampleArray[0]
-    for index in range(1,len(sampleArray)):
-        if sampleArray[index] > biggestNumber:
-            biggestNumber = sampleArray[index]
-    print(biggestNumber)
+def findbiggestnumber(samplearray):
+    biggestnumber = samplearray[0]
+    for index in range(1,len(samplearray)):
+        if samplearray[index] > biggestnumber:
+            biggestnumber = samplearray[index]
+    print(biggestnumber)
 
-findBiggestNumber(sample1Array)
+findbiggestnumber(sample1Array)
 
 ######  Recursive algorithm - finding the biggest number in the array ####### 
 
-def findMaxNumRec(sampleArray, n):
+def findmaxnumrec(samplearray, n):
     if n == 1:
-       return sampleArray[0]
-    return max(sampleArray[n-1],findMaxNumRec(sampleArray,n-1))
+       return samplearray[0]
+    return max(samplearray[n-1],findmaxnumrec(samplearray,n-1))
 
-print(findMaxNumRec(sample1Array,len(sample1Array)))
+print(findmaxnumrec(sample1Array,len(sample1Array)))
 
 
 ######  Recursive algorithm multiple calls ####### 
