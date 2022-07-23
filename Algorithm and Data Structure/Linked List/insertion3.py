ce5ef1ea8a7ea6699cefa31fa5d683090d8c34d7
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-23 19:27:21 +0300

    insertion

--- Algorithm and Data Structure/Linked List/insertion3.py
+++ Algorithm and Data Structure/Linked List/insertion3.py
@@ -1,36 +1,37 @@
 class Node:
-   def __init__(self, dataval=None):
-      self.dataval = dataval
-      self.nextval = None
+   def __init__(self, data=None):
+      self.data = data
+      self.next = None
+
 class SLinkedList:
    def __init__(self):
-      self.headval = None
+      self.head = None
 
 # Function to add node
-   def Inbetween(self,middle_node,newdata):
+   def inbetween(self,middle_node,newdata):
       if middle_node is None:
          print("The mentioned node is absent")
          return
 
-      NewNode = Node(newdata)
-      NewNode.nextval = middle_node.nextval
-      middle_node.nextval = NewNode
+      newnode = Node(newdata)
+      newnode.next = middle_node.next
+      middle_node.next = newnode
 
 # Print the linked list
    def listprint(self):
-      printval = self.headval
+      printval = self.head
       while printval is not None:
-         print (printval.dataval)
-         printval = printval.nextval
+         print (printval.data)
+         printval = printval.next
 
 list = SLinkedList()
-list.headval = Node("Mon")
+list.head = Node("Mon")
 e2 = Node("Tue")
 e3 = Node("Thu")
 
-list.headval.nextval = e2
-e2.nextval = e3
+list.head.next = e2
+e2.next = e3
 
-list.Inbetween(list.headval.nextval,"Fri")
+list.inbetween(list.head.next,"Fri")
 
 list.listprint()
